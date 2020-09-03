b6f563096e63fc19d27e9802313f96834d6f620a
Alex <alexandrsokolov@cock.li>
2020-09-04 02:47:09 +0300

    коментарии

--- source/dropnote/views.py
+++ source/dropnote/views.py
@@ -8,23 +8,26 @@ from .models import SnippetItem
 
 
 class IndexPage(CreateView):
+    """Страница создания заметки"""
     template_name = "dropnote/index.html"
     model = SnippetItem
     fields = ["text"]
 
 
 class ViewPage(DetailView):
+    """Страница просмотра"""
     template_name = "dropnote/view.html"
     content_type = "text/html"
     model = SnippetItem
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # добавляем в контекст абсолютный урл на заметку
         context["snippet_url"] = self.request.build_absolute_uri(self.object.get_absolute_url())
         return context
 
     def post(self, request, **kwargs):
-        """Ajax"""
+        """Запрос сюда приходит от JS, когда пользователь решил увидеть содержимое."""
         self.object = self.get_object()
         self.object.delete()
         context = self.get_context_data(object=self.object)
@@ -35,5 +38,6 @@ class ViewPage(DetailView):
         try:
             return super().dispatch(request, *args, **kwargs)
         except Http404:
+            # вместо отображения страницы с 404, лучше покажем уведомление.
             messages.add_message(request, messages.WARNING, 'Note not found!', extra_tags="alert-warning",)
             return HttpResponsePermanentRedirect(reverse("dropnote:index"))
