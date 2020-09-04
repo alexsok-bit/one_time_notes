b0f3cb3bce02692612967bac6dedff7bba56238f
Alex <alexandrsokolov@cock.li>
2020-09-04 15:39:19 +0300

    добавлена возможность создавать заметки через curl

--- source/dropnote/tests.py
+++ source/dropnote/tests.py
@@ -1,1 +1,19 @@
-# Create your tests here.
+import re
+import os
+from subprocess import check_output
+
+from django.test import TestCase
+
+os.environ.setdefault("DJANGO_HOST", "http://127.0.0.1:8000/")
+
+
+class TestMakeNote(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.host = os.environ["DJANGO_HOST"]
+        if self.host.endswith("/"):
+            self.host = self.host[:-1]
+
+    def test_make_note(self):
+        output = check_output(f"curl -X PUT -s -d 'Hello' {self.host}/", shell=True)
+        self.assertTrue(re.match(b"^" + self.host.encode() + b"/[0-9a-z]{6}/$", output), output)
--- source/dropnote/views.py
+++ source/dropnote/views.py
@@ -1,17 +1,36 @@
 from django.contrib import messages
 from django.http import Http404, HttpResponsePermanentRedirect
 from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import DetailView
 from django.views.generic import CreateView
 
 from .models import SnippetItem
 
 
+@method_decorator(csrf_exempt, name="dispatch")
 class IndexPage(CreateView):
     """Страница создания заметки"""
     template_name = "dropnote/index.html"
     model = SnippetItem
     fields = ["text"]
+
+    def put(self, request, *args, **kwargs):
+        self.template_name = "dropnote/snippet.html"
+        response = self.post(request, *args, **kwargs)
+        if self.object:
+            response_data = self.request.build_absolute_uri(self.object.get_absolute_url())
+        else:
+            response_data = response.context_data["form"].errors.as_text()
+        return self.render_to_response({"object": {"text": response_data}}, content_type="text/plain")
+
+    def get_form_class(self):
+        form_class = super().get_form_class()
+        if self.request.method.lower() in ["put"]:
+            form_class.base_fields["text"].disabled = True
+            self.initial = {"text": self.request.body.decode()}
+        return form_class
 
 
 class ViewPage(DetailView):
